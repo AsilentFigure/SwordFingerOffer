bcc20f801d461978de42f323487ab5cbace91034
AsilentFigure <junyaoy@outlook.com>
2023-03-21 10:01:31 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import com.solutions.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Divide divideTest = new Divide();
+        System.out.println(divideTest.divide(17, 5));
+    }
+}

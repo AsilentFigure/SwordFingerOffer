5e9629624e35908042c08dd64f95067fd3e2606d
AsilentFigure <junyaoy@outlook.com>
2023-03-21 16:09:52 +0800

    CountBits commit

--- src/com/solutions/CountBits.java
+++ src/com/solutions/CountBits.java
@@ -7,14 +7,33 @@ package com.solutions;
  */
 public class CountBits {
 
-    public int [] countBits(int num) {
+    public int [] countBits1(int num) {
         int[] result = new int[num + 1];
         for (int i = 0; i <= num; i++) {
             int j = i;
             while (j != 0) {
                 result[i]++;
+                result[i] = Integer.bitCount(i);
                 j = j & (j - 1);
             }
+        }
+
+        return result;
+    }
+
+    public int [] countBits2(int num) {
+        int [] result = new int[num + 1];
+        for (int i = 1; i <= num; i++) {
+            result[i] = result[i & (i - 1)] + 1;
+        }
+
+        return result;
+    }
+
+    public int [] countBits3(int num) {
+        int[] result = new int[num + 1];
+        for (int i = 1; i <= num; i++) {
+            result[i] = result[i >> 1] + (i & 1);
         }
 
         return result;
